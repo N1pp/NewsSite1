4bac23b96472de4f208628c527063e2476e84117
NepovinDA <dimanepovin00@gmail.com>
2020-03-05 16:58:31 +0400

    Old changes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'NewsController@index');
+Route::get('/', function () {
+    return view('test');
+});
 
 Auth::routes();
 Route::middleware('auth')->group(function () {
